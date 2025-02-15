225fc90f4a4be341ba1cd3c7ccae18bc0827d07b
Zain Ul Abdeen <zu4425@gmail.com>
2025-02-15 20:55:03 +0500

    day 56

--- angular19/src/app/app.routes.ts
+++ angular19/src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { DashbaordComponent } from './dashbaord/dashbaord.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
-{path:'Home', component:HomeComponent},
-{path:'About',component:AboutusComponent},
-{path:'dashboard',component:DashbaordComponent}
+{path:'',redirectTo:'Home',pathMatch:'full'},
+{path:'Home', component:HomeComponent,title:'home'},
+{path:'About',component:AboutusComponent , title:'About'},
+{path:'dashboard',component:DashbaordComponent , title:'Dashboard'},
+{path:'**',component:PagenotfoundComponent , title:'Page not found'}
 
 
 ];
